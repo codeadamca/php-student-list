6a03cd543c73c90d4721cdb4c917086bc81bf78e
Adam <thomasadam83@hotmail.com>
2023-03-22 10:39:54 -0400

    Added sample API call

---
+++ list.php
@@ -0,0 +1,65 @@
+<?php
+
+$connect = mysqli_connect(
+    'sql213.epizy.com', // host
+    'epiz_29666120', // username
+    'U9x2tTEPKECh', // password
+    'epiz_29666120_students' // database
+);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student list</title>
+</head>
+<body>
+
+    <h1>Student List</h1>
+
+    <?php
+
+    $query = 'SELECT *
+        FROM programs
+        ORDER BY name';
+    $programs = mysqli_query($connect, $query);
+
+    ?>
+
+    <?php while($program = mysqli_fetch_assoc($programs)): ?>
+
+        <h2><?=$program['name']?></h2>
+
+        <?php
+
+        $query = 'SELECT *
+            FROM students
+            WHERE program_id = '.$program['id'].'
+            ORDER BY last,first';
+        $students = mysqli_query($connect, $query);
+
+        ?>
+
+        <?php while($student = mysqli_fetch_assoc($students)): ?>
+
+            <h3><?=$student['first']?> <?=$student['last']?></h3>
+            
+            <a href="<?=$student['portfolio']?>"><?=$student['portfolio']?></a>
+
+            <a href="https://www.linkedin.com/sharing/share-offsite/?url=<?=urlencode($student['portfolio'])?>">
+                Share on LinkedIn
+            </a>
+
+            <br>
+
+        <?php endwhile; ?>
+
+        <hr>
+
+    <?php endwhile; ?>
+    
+</body>
+</html>
